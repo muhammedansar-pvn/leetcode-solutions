fe3ea3cd7b2a39b55adcee247dde8d14054aedcb
muhammedansar-pvn <ansarbinkunhimarakar@gmail.com>
2026-05-05 16:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (94.19%) - LeetSync

---
+++ 66-plus-one/plus-one.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let num = BigInt(digits.join(""));
+    num = num + 1n;
+    return num.toString().split("").map(Number);
+};
