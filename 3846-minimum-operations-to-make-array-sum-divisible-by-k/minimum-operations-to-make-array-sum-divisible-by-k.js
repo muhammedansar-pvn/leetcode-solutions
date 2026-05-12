e34bda9db1e9636f058aa40e447b21d64090dfd8
muhammedansar-pvn <ansarbinkunhimarakar@gmail.com>
2026-05-12 16:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (97.87%) - LeetSync

---
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+
+    let sum=0
+    for (let i=0 ; i<nums.length ; i++){
+        sum +=nums[i]
+    }
+
+    return sum%k
+    
+};
